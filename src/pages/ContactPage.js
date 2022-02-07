c263a738756b03b72172f3cd0519f45a8007df76
Alex.Ramirez <aleramirez201@gmail.com>
2022-02-06 22:31:22 -0600

    changed a few last minute items and added a refernce css

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -10,11 +10,10 @@ const ContactPage = () => {
           Contact
         </h2>
         <div className='section-content'>
-          <p>I am looking for job opportunities, and would love to connect!</p>
-          <p>
-            Send me an Email or connect on social media:
+          <p>I am looking for job opportunities, and would love to connect! 
+           <br />Have a look at my Socials, Email, or my Resume
           </p>
-          <div className='social-icons'>
+          <div className='project-social'>
             <a href='https://github.com/Alexander-Ramirez/' target="_blank" rel="noopener noreferrer">
               Github
             </a>
@@ -23,6 +22,9 @@ const ContactPage = () => {
             </a>
             <a href='mailto: aleramirez201@gmail.com'>
              Gmail
+            </a>
+            <a href='https://docs.google.com/document/d/1QibdvibK1iP5eEXiQMCVpYd3j9wxB5NCmRmui6bRlWo/edit?usp=sharing' target="_blank" rel="noopener noreferrer">
+             Resume
             </a>
           </div>
         </div>
